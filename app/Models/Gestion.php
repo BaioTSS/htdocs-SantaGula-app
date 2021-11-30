206e4fe771db3fcc8c2e4e1c73a264f8dd44a5c2
BaioTSS <baiotssar@gmail.com>
2021-11-30 17:06:54 -0300

    config mails

--- app/Models/Gestion.php
+++ app/Models/Gestion.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class gestion extends Model
+class Gestion extends Model
 {
     use HasFactory;
 }
